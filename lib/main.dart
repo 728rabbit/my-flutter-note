91902ac32bc79f542e53f03d9516853114e1b179
728rabbit <kaiyunchan2012@hotmail.com>
2025-05-13 18:32:42 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'package:devapp/core/base.dart';
 import 'package:devapp/core/helper.dart';
+import 'package:devapp/pages/home.dart';
+import 'package:devapp/pages/login.dart';
 import 'package:devapp/routes.dart';
 import 'package:flutter/material.dart';
 
@@ -37,7 +39,8 @@ class MyApp extends StatelessWidget {
     //print(authedUser);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: ((authedUser?.isNotEmpty == true) ?AppRoute.home:AppRoute.login),
+      //initialRoute: ((authedUser?.isNotEmpty == true) ?AppRoute.home:AppRoute.login),
+      home: ((authedUser?.isNotEmpty == true) ?HomePage():LoginScreen()),
       routes: AppRoute.staticRoutes,
       onGenerateRoute: AppRoute.dynamicRoutes,
     );
